2e07661ac937d29d87c3e81b37e18c713430ada4
l4hiru <saranath.rimlinger@gmail.com>
2025-06-25 17:42:22 +0200

    CSP departemental dataset correction

--- Departemental Controls.R
+++ Departemental Controls.R
@@ -87,35 +87,49 @@ diploma_data <- diploma_data %>%
 
 #C) CSP 
 
-csp_data <- csp_data %>%
-  # Filtrer les lignes avec nomdep vide
+csp_data_dep <- csp_data %>%
   filter(nomdep != "") %>%
-  # Sélectionner seulement les colonnes CSP de 1978 à 2022
+  # Sélectionner colonnes CSP + pact + chom (effectifs)
   select(
     dep, nomdep,
-    matches("^(agri|indp|cadr|pint|empl|ouvr|chom)(197[8-9]|19[8-9]\\d|20[0-2]\\d|2022)$")
+    matches("^(agri|indp|cadr|pint|empl|ouvr|pact|chom)(197[8-9]|19[8-9]\\d|20[0-2]\\d|2022)$")
   ) %>%
-  # Agréger au niveau départemental
+  # Agréger par département (en sommant les effectifs si plusieurs lignes par département)
   group_by(dep, nomdep) %>%
-  summarise(across(where(is.numeric), \(x) mean(x, na.rm = TRUE)), .groups = "drop") %>%
-  # Restructurer les données
+  summarise(across(where(is.numeric), sum, na.rm = TRUE), .groups = "drop") %>%
+  # Restructurer en long
   pivot_longer(
     cols = -c(dep, nomdep),
     names_to = c("CSP", "Year"),
-    names_pattern = "(agri|indp|cadr|pint|empl|ouvr|chom)(\\d{4})",
-    values_to = "value"
+    names_pattern = "(agri|indp|cadr|pint|empl|ouvr|pact|chom)(\\d{4})",
+    values_to = "count"
   ) %>%
   filter(Year >= 1978 & Year <= 2022) %>%
-  # Recoder les noms CSP
-  mutate(
-    CSP = paste0("part", CSP),
-    Year = factor(Year)
-  ) %>%
   pivot_wider(
     names_from = CSP,
-    values_from = value
+    values_from = count
   ) %>%
-  arrange(dep, Year)
+  # Calcul des parts en % par rapport à la population active (pact)
+  mutate(
+    part_agri = 100 * agri / pact,
+    part_indp = 100 * indp / pact,
+    part_cadr = 100 * cadr / pact,
+    part_pint = 100 * pint / pact,
+    part_empl = 100 * empl / pact,
+    part_ouvr = 100 * ouvr / pact,
+    # Pour info, taux de chômage aussi possible :
+    part_chom = 100 * chom / pact
+  )
+
+
+summary(csp_data_dep$part_pint)
+summary(csp_data_dep$part_agri)
+summary(csp_data_dep$part_indp)
+summary(csp_data_dep$part_empl)
+summary(csp_data_dep$part_cadr)
+summary(csp_data_dep$part_ouvr)
+summary(csp_data_dep$part_chom)
+
 
 #E) Income 
 
@@ -170,7 +184,7 @@ immi_data <- immi_data %>%
 #III) Parquet exportation 
 
 write_parquet(age_data, "age_data_dep.parquet")
-write_parquet(csp_data, "csp_data_dep.parquet")
+write_parquet(csp_data_dep, "csp_data_dep.parquet")
 write_parquet(diploma_data, "diploma_data_dep.parquet")
 write_parquet(income_data, "income_data_dep.parquet")
 write_parquet(immi_data, "immi_data_dep.parquet")
